e4f3581dcaeece8c4f0d6283d3946aea19056d1a
SupiiX <gergely.akisupka@stud.uni-obuda.hu>
2023-10-21 21:38:23 +0200

    create

---
+++ EQ66L2_HFT_2023241/EQ66L2_HFT_2023241/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EQ66L2_HFT_2023241
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+
+
+            }
+
+        }
+    }
+}
