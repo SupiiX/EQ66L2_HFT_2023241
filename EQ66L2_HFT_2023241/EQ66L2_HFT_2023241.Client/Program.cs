ec8850089eee209d75c2f27a53323da7c7ff24d4
Supii_Pc <gergely.akisupka@stud.uni-obuda.hu>
2023-10-24 13:43:07 +0200

    git line added

---
+++ EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EQ66L2_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //
+            Console.WriteLine("XD");
+
+        }
+    }
+}
