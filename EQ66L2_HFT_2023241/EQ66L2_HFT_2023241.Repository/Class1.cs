ad67b81e9c36c2ad4b1de9677d59bdf4fa51abb2
Supii_Pc <gergely.akisupka@stud.uni-obuda.hu>
2023-11-03 17:28:43 +0100

    Nugets installed

--- EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EQ66L2_HFT_2023241.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Repository/DbContex.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EQ66L2_HFT_2023241.Repository
+{
+    public class DBContext : DbContext
+    {
+
+       
+
+        
+
+    }
+}
