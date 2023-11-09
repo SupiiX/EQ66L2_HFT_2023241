3a73546bffce2111b9190a275ea72b1a10f01977
Supii_Pc <gergely.akisupka@stud.uni-obuda.hu>
2023-11-09 21:28:59 +0100

    update, Manufacture class added

---
+++ EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Models/Manufacturer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQ66L2_HFT_2023241.Models
+{
+    public class Manufacturer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int ManufacturerID { get; set; }
+
+        [StringLength(150)]
+
+        public string ManufacturerName { get; set;}
+
+
+        [StringLength(150)]
+
+        public string PlaceOf { get; set;}
+
+        [StringLength(150)]
+
+        public string ManufacturerDescription { get; set;}
+
+
+    }
+}
