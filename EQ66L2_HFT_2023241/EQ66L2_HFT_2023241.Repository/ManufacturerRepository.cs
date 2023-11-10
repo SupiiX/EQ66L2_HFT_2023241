2e7c7b34cca1e597d2981654d3c649ceb15c8e08
Supii_Pc <gergely.akisupka@stud.uni-obuda.hu>
2023-11-10 18:32:49 +0100

    Model repositories added

---
+++ EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Repository/ManufacturerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQ66L2_HFT_2023241.Repository
+{
+    public class ManufacturerRepository
+    {
+
+
+    }
+}
