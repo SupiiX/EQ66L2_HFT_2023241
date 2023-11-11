0a4b4e012ac24b01d0b64227083ff09e33b38270
Supii_Pc <gergely.akisupka@stud.uni-obuda.hu>
2023-11-11 18:20:32 +0100

    ManufacturerRepository class updated

--- EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Repository/ManufacturerRepository.cs
+++ EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Repository/ManufacturerRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EQ66L2_HFT_2023241.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,39 @@ using System.Threading.Tasks;
 
 namespace EQ66L2_HFT_2023241.Repository
 {
-    public class ManufacturerRepository
+    public class ManufacturerRepository : Reposit<Manufacturer>, IManufacturerRepository
     {
+        public ManufacturerRepository(DBContext dbContext) : base(dbContext)
+        {
+        }
+
+        public void Origin(int id, string County)
+        {
+            var Manufacturer = Read(id);
+
+            if (Manufacturer != null)
+            {
+                Manufacturer.PlaceOf = County;
+
+               dbContext.SaveChanges();
+
+            }
+            else
+            {
+                throw new Exception("Error ID not found");
+            }
 
 
+        }
+
+
+
+        public override Manufacturer Read(int id)
+        {
+
+            // exeption?
+
+            return ReadAll().FirstOrDefault(x =>x.ManufacturerID == id);
+        }
     }
 }
