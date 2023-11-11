c2ad10f852d61b3ca0fb885e93a4ef9bcbbe69ab
Supii_Pc <gergely.akisupka@stud.uni-obuda.hu>
2023-11-11 18:37:53 +0100

    OrderRepository class updates

--- EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Repository/OrderRepository.cs
+++ EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Repository/OrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EQ66L2_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,50 @@ using System.Threading.Tasks;
 
 namespace EQ66L2_HFT_2023241.Repository
 {
-    internal class OrderRepository
+    internal class OrderRepository : Reposit<Order>, IOrderRepository
     {
+        public OrderRepository(DBContext dbContext) : base(dbContext)
+        {
+        }
+
+        //private Order;
+
+        public void ChangeProduct(int id, int NewProductId)
+        {
+        var Order = Read(id);
+
+            if (Order != null) 
+            {
+                Order.ProductID = NewProductId;
+
+                dbContext.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Error ID not found");
+            }
+
+        }
+
+        public void ChangeQuantity(int id, int Quantity)
+        {
+            var Order = Read(id);
+
+            if (Order != null)
+            {
+                Order.Quantity = Quantity;
+
+                dbContext.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Error ID not found");
+            }
+        }
+
+        public override Order Read(int id)
+        {
+            return ReadAll().FirstOrDefault(x => x.OrderID == id);
+        }
     }
 }
