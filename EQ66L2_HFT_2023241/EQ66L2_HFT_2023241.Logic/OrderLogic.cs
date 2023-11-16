49fb6765a92139eeaf52de0c2909739368ea6a3a
Supii_Pc <gergely.akisupka@stud.uni-obuda.hu>
2023-11-16 21:10:33 +0100

    non crud methods added to orderlogic

--- EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Logic/OrderLogic.cs
+++ EQ66L2_HFT_2023241/EQ66L2_HFT_2023241.Logic/OrderLogic.cs
@@ -1,5 +1,6 @@
 ﻿using EQ66L2_HFT_2023241.Models;
 using EQ66L2_HFT_2023241.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -57,6 +58,95 @@ namespace EQ66L2_HFT_2023241.Logic
         }
 
         /// Non CRUD
+
+        public IEnumerable<object> MelyikOrszagbanGyartjakALegtöbbetvasarolttermeket()
+        {
+
+            return orderRepository.ReadAll().
+                        GroupBy(x => x.Product.Manufacturer.PlaceOf).
+                        OrderByDescending(x => x.Sum(x => x.Quantity)).
+                        Select(x => new
+                        {
+                            Country = x.Key,
+                            Quantity = x.Sum(x => x.Quantity),
+                            //Customer = x.Select(x => x.Customer.CustomerName)  // exept
+                        });
+
+        }
+
+        /// 
+
+        public IEnumerable<P> KikoltotteALegtobbPenzt()
+        {
+
+            //return orderRepository.ReadAll().GroupBy(x => x.Customer.CustomerID).OrderBy(x => x.Sum(x => x.Quantity))
+            //                .Select(x => new
+            //                {
+            //                    a = x.Key,
+
+            //                    b = x.Select(x => x.Customer.CustomerName).ToString() 
+
+            //                });
+            //
+
+
+            var quer = from order in orderRepository.ReadAll()
+                       group order by order.CustomerID into customerGroup
+                       let totalSpent = customerGroup.Sum(order => order.Product.Price * order.Quantity)
+                       orderby totalSpent descending
+                       select new P
+                       {
+                           Id = customerGroup.Key,
+                           //Name = customerGroup.FirstOrDefault(x => x.Customer?.CustomerName),
+                           Total = totalSpent
+                       };
+
+            return quer.ToList();
+
+        }
+
+        public class P
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Total { get; set; }
+        }
+
+
+        public int Query()
+        {
+            return orderRepository.ReadAll().Select(x => new
+            {
+                warrant = x.Product.Warranty_year,
+                name = x.Customer.CustomerName,
+                quan = x.Quantity,
+
+            }
+
+
+            ).Max(x => x.quan);
+
+
+        }
+
+        public IEnumerable<object> query()
+        {
+            // Először lekérdezzük a rendeléseket és a hozzájuk tartozó vevőket egy listába
+            var orders = orderRepository.ReadAll().Include(x => x.Customer).ToList();
+
+            // Majd a listán végzünk csoportosítást és kiválasztást
+            return orders.Select(x => x.Customer).GroupBy(x => x.CustomerID).Select(x => new
+            {
+                c = x.Key,
+                a = x.FirstOrDefault().CustomerName,
+            });
+
+
+        }
+
+
+
+
 
 
     }
